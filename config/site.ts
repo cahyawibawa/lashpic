3e5129f030f9c56232bfc5469247b33fe758e45d
eternal sunshine <62229971+cahyawibawa@users.noreply.github.com>
2023-12-22 17:27:43 +0700

    feat: change name

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: 'Splasearcy',
+  name: 'Lashpic',
   description:
     'Beautifully designed components built with Radix UI and Tailwind CSS.',
   mainNav: [
